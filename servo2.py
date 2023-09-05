0e9b84f7a31415e7c5649ed52a778aff1297224a
nwashin59 <nwashin59@charlottesvilleschools.org>
2023-09-05 15:56:51 -0400

    got buttons working on servo2

---
+++ servo2.py
@@ -0,0 +1,39 @@
+import time
+import board
+import pwmio
+from digitalio import DigitalInOut, Direction, Pull
+from adafruit_motor import servo
+btn = DigitalInOut(board.D3)
+btn.direction = Direction.INPUT
+btn.pull = Pull.DOWN
+
+btn2 = DigitalInOut(board.D4)
+btn2.direction = Direction.INPUT
+btn2.pull = Pull.DOWN
+
+pwm = pwmio.PWMOut(board.A2, duty_cycle=2 ** 15, frequency=50)
+my_servo = servo.Servo(pwm)
+
+angle = 90
+
+while True:
+    if btn.value:
+        print("BTN1 is down")
+        angle = angle + 5
+        my_servo.angle = angle
+   
+
+    if btn2.value:
+        print("BTN2 is down")
+        angle = angle - 5
+        my_servo.angle = angle
+   
+    '''
+for angle in range(0, 180, 5):  # 0 - 180 degrees, 5 degrees at a time.
+        my_servo.angle = angle
+        time.sleep(0.02)
+    for angle in range(180, 0, -5): # 180 - 0 degrees, 5 degrees at a time.
+        my_servo.angle = angle
+        time.sleep(0.02)
+   '''     
+    time.sleep(0.1) # sleep for debounce
